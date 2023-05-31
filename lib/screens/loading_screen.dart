d209892c612c09021565979c52e6bad1f6791d6d
alriyadsouza <alriyatreeza929@gmail.com>
2023-05-31 09:50:15 +0530

    added location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -8,21 +8,32 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+  @override
+  void initState(){
+    super.initState();
+    getLocation();
   }
+  void getLocation() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+
+    if (permission == LocationPermission.denied) {
+      // Handle the case when the user denies permission
+      print('Location permission denied');
+    } else if (permission == LocationPermission.deniedForever) {
+      // Handle the case when the user denies permission permanently
+      print('Location permission permanently denied');
+    } else {
+      // Permission granted or already granted
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low,
+      );
+      print(position);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
     );
   }
 }
